b95765299ed27079eadcae0d80eb9f09078a1aa2
Behlül Savaskurt <behlul.savaskurt@student.hogent.be>
2026-09-05 09:39:35 +0200

    Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://be-skrt.github.io',
+  base: '/De-Parelvisser'
+});
