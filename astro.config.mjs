054729478f9102a9c10d52ced594e9b9cc748d93
Behlül Savaskurt <behlul.savaskurt@student.hogent.be>
2026-09-12 08:54:10 +0200

    trailing slash never config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://be-skrt.github.io',
-  base: '/De-Parelvisser'
+  base: '/De-Parelvisser',
+  trailingSlash: 'never',
 });
